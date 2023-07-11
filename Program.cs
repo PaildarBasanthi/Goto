b280d19ec85e79fe32ef1064c21340a7732bd7df
PaildarBasanthi <basanthisrec@gmail.com>
2023-07-11 11:59:04 +0530

    first Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gotoexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            Console.WriteLine("Enter user name");
+            username= Console.ReadLine();
+            if(username.Length>=6 ) {
+                Console.WriteLine("Welcome : \t " + username);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid username");
+            }
+            Console.ReadKey();
+        }
+    }
+}
